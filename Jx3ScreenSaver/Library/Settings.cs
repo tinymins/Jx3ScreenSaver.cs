6200eed4c5aedb161142a93d736d00e41dfe7194
Zhai YiMing <tinymins@gmail.com>
2016-02-21 15:28:13 +0800

    Save data to roaming in order to prevent from 360 killer...

--- Jx3ScreenSaver/Library/Settings.cs
+++ Jx3ScreenSaver/Library/Settings.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Win32;
+﻿using System;
+using System.IO;
+using System.Xml;
 using System.Collections.Generic;
 
 namespace Jx3ScreenSaver
@@ -7,23 +9,86 @@ namespace Jx3ScreenSaver
     {
         // Cache
         private static Dictionary<string, string> dicCache = new Dictionary<string,string>();
+        private static string xmlRoot = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\TmsJx3ScreenSaver\\";
+        private static string xmlPath = xmlRoot + "config.xml";
+        private static XmlDocument xmlDoc = null;
+
+        // Load xml file
+        private static void LoadXml()
+        {
+            if (xmlDoc == null)
+            {
+                xmlDoc = new XmlDocument();
+                if (File.Exists(xmlPath))
+                {
+                    xmlDoc.Load(xmlPath);
+                }
+                if (xmlDoc.SelectSingleNode("config") == null)
+                {
+                    xmlDoc.AppendChild(xmlDoc.CreateNode(XmlNodeType.Element, "config", null));
+                }
+            }
+        }
+
+        // Save xml file
+        private static void SaveXml()
+        {
+            if (xmlDoc != null)
+            {
+                if (!Directory.Exists(xmlRoot))
+                {
+                    Directory.CreateDirectory(xmlRoot);
+                }
+                xmlDoc.Save(xmlPath);
+            }
+        }
 
         // Set setting value by key and value
         private static void Set(string key, string value)
         {
+            LoadXml();
+            bool isfind = false;
+            XmlNode root = xmlDoc.SelectSingleNode("config");
+            XmlNodeList nodeList = root.ChildNodes;
+            foreach (XmlNode xn in nodeList)
+            {
+                XmlElement xe = (XmlElement)xn;
+                if (xe.GetAttribute("key") == key)
+                {
+                    xe.SetAttribute("value", value);
+                    isfind = true;
+                    break;
+                }
+            }
+            if (!isfind)
+            {
+                XmlElement xe1 = xmlDoc.CreateElement("cfg");
+                xe1.SetAttribute("key", key);
+                xe1.SetAttribute("value", value);
+                root.AppendChild(xe1);
+            }
+            SaveXml();
+
             dicCache[key] = value;
-            Registry.CurrentUser.CreateSubKey("SOFTWARE\\TmsJx3ScreenSaver").SetValue(key, value);
         }
 
         // Get setting value by key and default value
         private static string Get(string key, string defaultVal)
         {
+            LoadXml();
             if (!dicCache.ContainsKey(key))
             {
-                RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\TmsJx3ScreenSaver");
-
-                if (rk != null)
-                    dicCache[key] = (string)rk.GetValue(key);
+                XmlNode root = xmlDoc.SelectSingleNode("config");
+                XmlNodeList nodeList = root.ChildNodes;
+                foreach (XmlNode xn in nodeList)
+                {
+                    XmlElement xe = (XmlElement)xn;
+                    if (xe.GetAttribute("key") == key)
+                    {
+                        dicCache[key] = xe.GetAttribute("value");
+                        break;
+                    }
+                }
 
                 if (!dicCache.ContainsKey(key))
                     dicCache[key] = defaultVal;
